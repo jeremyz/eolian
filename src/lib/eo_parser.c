1e6982068f89a99260cec8fbe1507c986aa41ecf
Yakov Goldberg <yakov.g@samsung.com>
2013-10-15 15:42:31 +0300

    add func to parse generated .eo + example file

--- src/lib/eo_parser.c
+++ src/lib/eo_parser.c
@@ -92,6 +92,101 @@ end:
    return foo_id;
 }
 
+static char*
+_strip(char* str)
+{
+   char *beg, *end;
+   beg = str;
+   end = str + strlen(str) - 1;
+
+   while (*beg == ' ')
+     beg++;
+   while (*end == ' ')
+     end--;
+   return strndup(beg, end - beg + 1);
+}
+
+static Function_Id
+_function_parse2(char *buffer, char **new_buffer)
+{
+   Function_Id foo_id = NULL;
+   *new_buffer = NULL;
+   /*
+    * COMMENT = STRING
+    * TYPE = STRING
+    * TYPES = | TYPE | TYPE TYPES
+    * PARAM = TYPE PARAM_NAME COMMENT | TYPE PARAM_NAME
+    * PARAMS = | PARAM | PARAM PARAMS
+    * PROTOTYPE = TYPES FUNC_NAME ( PARAMS )
+    * FUNCTION = COMMENT PROTOTYPE | PROTOTYPE
+    */
+
+   // COMMENT
+   char *comment = NULL;
+   char *tmp_buffer = _comment_parse(buffer, &comment);
+   if (tmp_buffer) buffer = tmp_buffer;
+
+   // PROTOTYPE
+   char *types_function = NULL; // types and function, no params
+   tmp_buffer = LEX(buffer, STRING("(", &types_function));
+   if (!types_function) goto end;
+
+   // FUNC_NAME
+   char *function = NULL;
+   LEX_REVERSE(types_function, strlen(types_function), UWORD(&function));
+   foo_id = database_function_new(function);
+   if (comment)
+      database_function_description_set(foo_id, comment);
+
+   // Return TYPES
+   types_function[strlen(types_function) - strlen(function)] = '\0'; // needed to parse the types
+   Eina_List *types_list = NULL, *itr;
+   char *type_as_string;
+   LEX(types_function, STRINGS_LIST(",", &types_list));
+   EINA_LIST_FOREACH(types_list, itr, type_as_string)
+      database_function_parameter_add(foo_id, EINA_FALSE, type_as_string, NULL, NULL);
+
+   // PARAMS. tmp_buffer points to them
+   char *params = NULL;
+   Eina_List *params_list = NULL;
+   *new_buffer = LEX(tmp_buffer, STRING(");", &params));
+   LEX(params, STRINGS_LIST(",", &params_list));
+   EINA_LIST_FOREACH(params_list, itr, type_as_string)
+     {
+        char *type_comment = strstr(type_as_string, "/*");
+        char *type_comment2 = NULL;
+        if (type_comment)
+          {
+             _comment_parse(type_comment, &type_comment2);
+             type_as_string[strlen(type_as_string) - strlen(type_comment)] = '\0';
+          }
+        char *name = NULL;
+        char *dir = NULL;
+        char *type = NULL;
+        LEX(type_as_string, UWORD(&dir)); // extract the param name
+        LEX_REVERSE(type_as_string, strlen(type_as_string), UWORD(&name)); // extract the param name
+        char *tmp = strstr(type_as_string, name);
+        *tmp = '\0';
+        type = _strip(type_as_string + strlen(dir));
+        database_function_parameter_add(foo_id, !strcmp(dir, "in"), type, name, type_comment2);
+        if (name) free(name);
+        if (dir) free(dir);
+        if (type) free(type);
+     }
+
+end:
+   if (comment)
+      free(comment);
+   if (types_function)
+      free(types_function);
+   if (function)
+      free(function);
+   EINA_LIST_FREE(types_list, type_as_string)
+      free(type_as_string);
+
+   return foo_id;
+}
+
 static char *
 _class_parse(char *buffer)
 {
@@ -142,6 +237,40 @@ _class_parse(char *buffer)
                          }
                     }
                }
+             if(!strcmp(token, "properties_set"))
+               {
+                  new_buffer = LEX(buffer, KWORD("properties_set"), KCHAR('{'));
+                  Eina_Bool loop = EINA_TRUE;
+                  while (new_buffer && loop)
+                    {
+                       buffer = new_buffer;
+                       Function_Id foo_id = _function_parse(buffer, &new_buffer);
+                       if (foo_id)
+                          database_class_property_add(class_name, foo_id);
+                       if (LEX(new_buffer, KCHAR('}')))
+                         {
+                            new_buffer = LEX(new_buffer, KCHAR('}'));
+                            loop = EINA_FALSE;
+                         }
+                    }
+               }
+             if(!strcmp(token, "properties_get"))
+               {
+                  new_buffer = LEX(buffer, KWORD("properties_get"), KCHAR('{'));
+                  Eina_Bool loop = EINA_TRUE;
+                  while (new_buffer && loop)
+                    {
+                       buffer = new_buffer;
+                       Function_Id foo_id = _function_parse(buffer, &new_buffer);
+                       if (foo_id)
+                          database_class_property_add(class_name, foo_id);
+                       if (LEX(new_buffer, KCHAR('}')))
+                         {
+                            new_buffer = LEX(new_buffer, KCHAR('}'));
+                            loop = EINA_FALSE;
+                         }
+                    }
+               }
              if(!strcmp(token, "methods"))
                {
                   new_buffer = LEX(buffer, KWORD("methods"), KCHAR('{'));
@@ -149,7 +278,7 @@ _class_parse(char *buffer)
                   while (new_buffer && loop)
                     {
                        buffer = new_buffer;
-                       Function_Id foo_id = _function_parse(buffer, &new_buffer);
+                       Function_Id foo_id = _function_parse2(buffer, &new_buffer);
                        if (foo_id)
                           database_class_method_add(class_name, foo_id);
                        if (LEX(new_buffer, KCHAR('}')))
