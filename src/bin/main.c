ae7679ceef450dace26ee938e78561f1141a9f4e
Yakov Goldberg <yakov.g@samsung.com>
2013-10-21 13:35:39 +0300

    add support for multiple files and dirs

--- src/bin/main.c
+++ src/bin/main.c
@@ -1,11 +1,15 @@
 #include <Eina.h>
+#include <Ecore_File.h>
 #include "Eolian.h"
+
+#define EO_SUFFIX ".eo"
 
 int main(int argc, char **argv)
 {
+   eina_init();
    int i;
    Eina_Bool help = EINA_FALSE, show = EINA_FALSE;
-   char *input_file = NULL; // FIXME: for the moment, only one
+   Eina_List *files = NULL, *itr;
    for(i = 1; i < argc; i++)
      {
         if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
@@ -18,26 +22,76 @@ int main(int argc, char **argv)
              show = EINA_TRUE;
              continue;
           }
-        input_file = argv[i];
+        /* Directory parameter found. */
+        if (!strcmp(argv[i], "-d") && (i < (argc-1)))
+          {
+             i++;
+             char *dir = ecore_file_realpath(argv[i]);
+             if (strlen(dir) != 0)
+               {
+                  if (ecore_file_is_dir(dir))
+                    {
+                       Eina_List *dir_files;
+                       char *file;
+                       /* Get all files from directory. Not recursively!!!*/
+                       dir_files = ecore_file_ls(dir);
+                       EINA_LIST_FREE(dir_files, file)
+                         {
+                            char *filepath = malloc(strlen(dir) + 1 + strlen(file) + 1);
+                            sprintf(filepath, "%s/%s", dir, file);
+                            if ((!ecore_file_is_dir(filepath)) && eina_str_has_suffix(filepath, EO_SUFFIX))
+                              {
+                                 /* Allocated string will be freed during deletion of "files" list. */
+                                 files = eina_list_append(files, strdup(filepath));
+                              }
+                            free(filepath);
+                            free(file);
+                         }
+                    }
+               }
+             free(dir);
+             continue;
+          }
+        if (!strcmp(argv[i], "-f") && (i < (argc-1)))
+          {
+             i++;
+             char *realpath = ecore_file_realpath(argv[i]);
+             if (strlen(realpath) != 0)
+               {
+                  if (!ecore_file_is_dir(realpath))
+                    {
+                       if (eina_str_has_suffix(realpath, EO_SUFFIX))
+                         files = eina_list_append(files, strdup(realpath));
+                    }
+               }
+             free(realpath);
+             continue;
+          }
      }
 
-   if (!input_file || help)
+   if (!files || help)
      {
         printf("Usage: %s [-h/--help] [--show] input files\n", argv[0]);
         return 0;
      }
 
    eolian_database_init();
-   if (!eolian_eo_file_parse(input_file))
+   char *filename;
+   EINA_LIST_FOREACH(files, itr, filename)
      {
-        printf("Error during parsing of file %s\n", input_file);
-        return 1;
+        if (!eolian_eo_file_parse(filename))
+          {
+             printf("Error during parsing file %s\n", filename);
+          }
      }
 
    if (show)
      {
         eolian_show();
      }
+   EINA_LIST_FREE(files, filename)
+      free(filename);
    eolian_database_shutdown();
+   eina_shutdown();
    return 0;
 }
